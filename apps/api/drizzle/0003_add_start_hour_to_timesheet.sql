a24e42ff5c9c82e196bbfbfcb4621a55b59635cb
Gimbler778 <decoder160@gmail.com>
2026-04-28 19:35:08 +0530

    fixed db migration issues

--- apps/api/drizzle/0003_add_start_hour_to_timesheet.sql
+++
@@ -1,2 +0,0 @@
--- Add startHour column to timesheet_entry table
-ALTER TABLE "timesheet_entry" ADD COLUMN "start_hour" real DEFAULT 9;
---
+++ apps/api/drizzle/0005_add_start_hour_to_timesheet.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "timesheet_entry" ADD COLUMN "start_hour" real DEFAULT 9 NOT NULL;
